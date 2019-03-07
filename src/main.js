16ddee33aa90fb919a3993137ba5851c22eba6d1
zhangyushi <1193822368@qq.com>
2019-03-07 19:41:01 +0800

    store is changed(I can't)

--- src/main.js
+++ src/main.js
@@ -11,11 +11,48 @@ Vue.use(ElementUI)
 Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    products: [
+      {name: '鼠标', price: 20},
+      {name: '键盘', price: 40},
+      {name: '耳机', price: 60},
+      {name: '显示屏', price: 80}
+    ]
+  },
+  getters: {
+    saleProducts: (state) => {
+      let saleProducts = state.products.map(product => {
+        return {
+          name: product.name,
+          price: product.price / 2
+        }
+      })
+      return saleProducts
+    }
+  },
+  mutations: {
+    minusPrice (state, payload) {
+      let newPrice = state.products.forEach(product => {
+        product.price -= payload
+      })
+      return newPrice
+    }
+  },
+  actions: {
+    minusPriceAsync (context, payload) {
+      return setTimeout(() => {
+        context.commit('minusPrice', payload)
+      }, 2000)
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  // store,
+  store,
   // render: h => h(App),
   components: { App },
   template: '<App/>'
