82c338510b58fbb077d5cbd2120758fb4bea98a9
zhangyushi <1193822368@qq.com>
2019-03-07 20:51:55 +0800

    reject

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,9 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path: '/', redirect: '/login'},
     {
-      path: '/',
+      path: '/vuex',
       name: 'HelloWorld',
       component: HelloWorld
     },
